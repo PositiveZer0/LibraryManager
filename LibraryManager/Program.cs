26b7346639ee031d380aeb5c8e4c949e2e76657d
PositiveZer0 <60852918+PositiveZer0@users.noreply.github.com>
2022-01-11 21:39:40 +0200

    Add repository pattern

--- LibraryManager/Program.cs
+++ LibraryManager/Program.cs
@@ -1,9 +1,12 @@
 namespace LibraryManager
 {
     using System;
+    using System.Threading.Tasks;
     using System.Windows.Forms;
 
     using LibraryManager.Database.Data;
+    using LibraryManager.Database.Models;
+    using LibraryManager.SendGrid;
 
     static class Program
     {
@@ -13,10 +16,16 @@ namespace LibraryManager
         [STAThread]
         static void Main()
         {
-            //Ensure that database is created
-            var db = new LibraryManagerContext();
-            db.Database.EnsureCreated();
-
+            ////Ensure that database is created
+            //var db = new LibraryManagerContext();
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+            //var book = new Book() { Author = "Steven", Name = "MyFirstBook", Year = 1999 };
+            //db.Books.Add(book);
+            //db.SaveChanges();
+            ////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
+            //var sendGrid = new SendGridEmailSender("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");
+            //await sendGrid.SendEmailAsync("ghostinthewires@abv.bg", "emo", "azsumemi@gmail.com", "test", "First email send");
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
