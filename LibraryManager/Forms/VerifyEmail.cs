ba17ebe614de471a421ae8d29bfda060dd712cb2
PositiveZer0 <60852918+PositiveZer0@users.noreply.github.com>
2022-01-25 01:14:29 +0200

    Create verify email form

---
+++ LibraryManager/Forms/VerifyEmail.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryManager.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Text;
+    using System.Windows.Forms;
+
+    public partial class VerifyEmail : Form
+    {
+        public VerifyEmail()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- LibraryManager/Properties/Resources.Designer.cs
+++ LibraryManager/Properties/Resources.Designer.cs
@@ -99,5 +99,25 @@ namespace LibraryManager.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap verification {
+            get {
+                object obj = ResourceManager.GetObject("verification", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap verificationEmail {
+            get {
+                object obj = ResourceManager.GetObject("verificationEmail", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
