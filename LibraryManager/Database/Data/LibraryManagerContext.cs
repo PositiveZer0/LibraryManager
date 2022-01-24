a12991f61fa8569c3f463debe75ee34869063f0d
PositiveZer0 <60852918+PositiveZer0@users.noreply.github.com>
2022-01-24 15:32:14 +0200

    Small changes in dbContext

--- LibraryManager/Database/Data/LibraryManagerContext.cs
+++ LibraryManager/Database/Data/LibraryManagerContext.cs
@@ -1,35 +1,121 @@
 ﻿namespace LibraryManager.Database.Data
 {
     using LibraryManager.Database.Models;
+    using LibraryManager.Database.Repositories;
+    using LibraryManager.Database.Repositories.Models;
     using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
 
-    
-	public class LibraryManagerContext : DbContext
-{
-    public LibraryManagerContext()
+    public class LibraryManagerContext : DbContext
     {
-    }
+        private static readonly MethodInfo SetIsDeletedQueryFilterMethod =
+            typeof(LibraryManagerContext).GetMethod(
+                nameof(SetIsDeletedQueryFilter),
+                BindingFlags.NonPublic | BindingFlags.Static);
 
-    public LibraryManagerContext(DbContextOptions options)
-        : base(options)
-    {
-    }
+        public LibraryManagerContext()
+        {
+        }
 
-    public virtual DbSet<User> Users { get; set; }
+        public LibraryManagerContext(DbContextOptions options)
+            : base(options)
+        {
+        }
 
-    public virtual DbSet<ConfirmEmail> ConfirmEmails { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<ConfirmEmail> ConfirmEmails { get; set; }
+
+        public override int SaveChanges() => this.SaveChanges(true);
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) =>
+            this.SaveChangesAsync(true, cancellationToken);
+
+        public override Task<int> SaveChangesAsync(
+            bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default)
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Needed for Identity models configuration
+            base.OnModelCreating(builder);
+
+            this.ConfigureUserIdentityRelations(builder);
+
+            EntityIndexesConfiguration.Configure(builder);
+
+            var entityTypes = builder.Model.GetEntityTypes().ToList();
+
+            // Set global query filter for not deleted entities only
+            var deletableEntityTypes = entityTypes
+                .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+            foreach (var deletableEntityType in deletableEntityTypes)
+            {
+                var method = SetIsDeletedQueryFilterMethod.MakeGenericMethod(deletableEntityType.ClrType);
+                method.Invoke(null, new object[] { builder });
+            }
+
+            // Disable cascade delete
+            var foreignKeys = entityTypes
+                .SelectMany(e => e.GetForeignKeys().Where(f => f.DeleteBehavior == DeleteBehavior.Cascade));
+            foreach (var foreignKey in foreignKeys)
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        private static void SetIsDeletedQueryFilter<T>(ModelBuilder builder)
+            where T : class, IDeletableEntity
+        {
+            builder.Entity<T>().HasQueryFilter(e => !e.IsDeleted);
+        }
+
+        // Applies configurations
+        private void ConfigureUserIdentityRelations(ModelBuilder builder)
+             => builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder
-                .UseSqlServer(Configuration.ConnectionString);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+        private void ApplyAuditInfoRules()
+        {
+            var changedEntries = this.ChangeTracker
+                .Entries()
+                .Where(e =>
+                    e.Entity is IAuditInfo &&
+                    (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            foreach (var entry in changedEntries)
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default)
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
         }
     }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-    }
-}
 }
