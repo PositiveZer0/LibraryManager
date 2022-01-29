de07957273aa08578598921f1abacf601e7cb3ec
Emil <azsumemi@gmail.com>
2022-01-29 16:50:46 +0200

    Small changes

--- LibraryManager/Database/Data/Configuration.cs
+++ LibraryManager/Database/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @$"{ConfigurationConstants.SERVER_NAME};Database=LibraryManager;Trusted_Connection=True";
+        public static string ConnectionString = @$"Data Source={ConfigurationConstants.SERVER_NAME};Database=LibraryManager;Trusted_Connection=True";
     }
 }
